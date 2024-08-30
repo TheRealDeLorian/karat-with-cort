2ae12cdabae1e661f18dd2dc82f15132ef9be1f4
Dorian C <thenewdorian21@gmail.com>
2024-08-29 23:51:05 -0600

    I GOT IT!

--- k.py
+++ k.py
@@ -1,24 +1,20 @@
 import math
 
-def count_digits(n):
-    if n == 0:
-        return 1
-    n = abs(n)
-    return math.floor(math.log10(n)) + 1
-        
 def karat(x, y):
         if x < 10 and y < 10:
                 return x*y
         
-        # print(f'x: {x}\ny: {y}\n')
-        n = count_digits(x if x > y else y) # number of digits
+        print(f'x: {x}\ny: {y}\n')
+        n = len(str(x if x > y else y)) # number of digits
         base = 10 # base 10
-        a = x // (10 ** (n // 2)) 
-        b = x % (10 ** (n // 2))
-        c = y // (10 ** (n // 2))
-        d = y % (10 ** (n // 2))
+        m = math.ceil(n / 2)
+        # print(f'n: {n}\nm: {m}')
+        a = x // (10 ** m) 
+        b = x % (10 ** m)
+        c = y // (10 ** m)
+        d = y % (10 ** m)
         
-        # print(f'n: {n}\nbase: {base}\na: {a}\nb: {b}\nc: {c}\nd: {d}\n')
+        print(f'm: {m}\nn: {n}\nbase: {base}\na: {a}\nb: {b}\nc: {c}\nd: {d}\n')
         
         #step 1: e = a*c
         e = a*c
@@ -28,16 +24,20 @@ def karat(x, y):
         g = (a+b)*(c+d)
         #4: h = g-e-f
         h = g-e-f
-        # print(f'e: {e}\nf: {f}\ng: {g}\nh: {h}\n')
+        print(f'e: {e}\nf: {f}\ng: {g}\nh: {h}\n')
         #5: (10^4) *e + (10^2) * h + f 
-        return (base**n) * e + (base**(n//2)) * h + f
+        # print(f'e: {e}')
+        # print(base**n)
+        # print(h)
+        # print(base**m)
+        # print(f)
+        return (e*(base**m)**2)  + (h*(base**m)) + f
        
 
 print(4321 // 100) #100 is 10^2
-print(count_digits(4321))
 
 print(karat(1234, 5678)) #expected: 7006652
-print(karat(12345, 67890)) # 838102050
+print(karat(12345, 6789)) # 83810205
 print(karat(123456, 789123)) # 97421969088
 print(karat(12345678910, 10987654321)) # 1.3565005e+20
 
