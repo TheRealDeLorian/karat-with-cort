671c5b1ca67dc912cd92c4948f874e888396ca1b
Cortlynd101 <CortlyndCox@gmail.com>
2024-09-03 15:22:35 -0600

    Added tests using pytest.

--- k.py
+++ k.py
@@ -4,17 +4,13 @@ def karat(x, y):
         if x < 10 and y < 10:
                 return x*y
         
-        # print(f'x: {x}\ny: {y}\n')
         n = len(str(x if x > y else y)) # number of digits
         base = 10 # base 10
         m = math.ceil(n / 2)
-        # print(f'n: {n}\nm: {m}')
         a = x // (base ** m) 
         b = x % (base ** m)
         c = y // (base ** m)
         d = y % (base ** m)
-        
-        # print(f'm: {m}\nn: {n}\nbase: {base}\na: {a}\nb: {b}\nc: {c}\nd: {d}\n')
         
         #step 1: e = a*c
         e = karat(a,c)
@@ -24,10 +20,7 @@ def karat(x, y):
         g = karat(a+b, c+d)
         #step 4: h = g-e-f
         h = g-e-f
-        # print(f'e: {e}\nf: {f}\ng: {g}\nh: {h}\n')
         #step 5: (10^4) *e + (10^2) * h + f 
-        # print(f"e: {e}, h: {h} ")
-        basepowm = (base**m)
 
         for x in range (m*2):
                 e = (e<<3) + (e<<1) #multiplies by 10 once.
@@ -39,8 +32,20 @@ def karat(x, y):
         return (e + h) + f
        
 
-print(karat(1234, 5678)) #expected: 7006652
-print(karat(12345, 6789)) # 83810205
-print(karat(123456, 789123)) # 97421969088
-print(karat(12345678910, 10987654321)) # 1.3565005e+20
+print(karat(1234, 5678)) #Expected: 7006652
+print(karat(12345, 6789)) #Expected: 83810205
+print(karat(123456, 789123)) #Expected: 97421969088
+print(karat(12345678910, 10987654321)) #Expected: 135650052221140070110
 # print(karat(3141592653589793238462643383279502884197169399375105820974944592, 2718281828459045235360287471352662497757247093699959574966967627))
+
+def test_first ():
+    assert karat(1234, 5678) == 7006652 #Expected: 7006652
+
+def test_second ():
+        assert karat(12345, 6789) == 83810205 #Expected: 83810205
+
+def test_third ():
+        assert karat(123456, 789123) == 97421969088 #Expected: 97421969088
+
+def test_fourth ():
+        assert karat(12345678910, 10987654321) == 135650052221140070110 #Expected: 135650052221140070110
